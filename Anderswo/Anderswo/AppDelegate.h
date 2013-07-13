687c0ffc6c8a0a2a63e39523535f3a06db4362af
Ida Buchwald <ida91@web.de>
2013-07-13 15:53:03 +0200

    New project for Anderswo

---
+++ Anderswo/Anderswo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Anderswo
+//
+//  Created by Ida Buchwald on 13.07.13.
+//  Copyright (c) 2013 Tidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestFlight.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
